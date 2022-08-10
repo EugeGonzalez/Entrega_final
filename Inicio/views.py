586284ab15fd12d51036ed709a7dc232a09a279a
Mateosalinas <108773833+Mateosalinas@users.noreply.github.com>
2022-08-09 23:02:50 -0500

    Solucion blog, nosotros

--- Inicio/views.py
+++ Inicio/views.py
@@ -6,18 +6,18 @@ from Posts.models import Post
 
 def inicio (request): 
     if request.user.is_authenticated:
-        return render(request, "Inicio/index.html",{'user_avatar':buscar_url_avatar(request.user)}) 
+        return render(request, "Inicio/index.html") 
     else:
         return render(request, "Inicio/index.html")
 
-@login_required
+
 def nosotros (request): 
-    return render (request,'Inicio/nosotros.html',{'user_avatar':buscar_url_avatar(request.user)})
+    return render (request,'Inicio/nosotros.html')
 
 
-@login_required
+
 def blog (request):
     posts = Post.objects.all()
-    return render(request, "Inicio/blog.html",{'posts': posts, 'user_avatar':buscar_url_avatar(request.user)}) 
+    return render(request, "Inicio/blog.html") 
 
 
