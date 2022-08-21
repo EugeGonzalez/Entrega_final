4379a755e001bc17f4ccc1b438d1c58c07363a25
Euge Gonzalez <gonzalezm.euge97@gmail.com>
2022-08-20 23:45:20 -0300

    intento de solucion del post, se publica pero no logro verlo

--- Posts/urls.py
+++ Posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('formulario_post/', views.formulario_posteo, name='formulario_post'),
-    path('lista_post/', views.lista_post, name='Lista_posts'),
+    path('lista_post/', views.lista_post, name='lista_posts'),
     path(r'^delete_post/(?P<pk>\d+)/$', views.DeletePost.as_view(), name='delete_posts'),
     path(r'^detalle_post/(?P<pk>\d+)/$', views.DetallePost.as_view(), name='detalle_posts'),
 ]
